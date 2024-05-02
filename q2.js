4260ca362351e0eee3b056141329955a0b13a299
navah21 <164158949+navah21@users.noreply.github.com>
2024-05-02 12:49:52 +0530

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,6 @@
+let arr1=[1,2,3]
+let arr2=[4,5]
+
+let c=arr1.concat(arr2);
+
+console.log(c)
---
+++ uni.js
@@ -0,0 +1,8 @@
+let arr=[1.2,3,4,5]
+let a=[];
+let c =0;
+for(let i=1;i<arr.length+1;i++){
+    let c =a+[i]
+  
+}
+
